20f124db8b915931bfe076a72bfc7c707a147e51
baorant <779074463@qq.com>
2022-07-16 11:02:07 +0800

    [修改说明] 应用设置crashhandler 1、crash发生进行兜底拦截重启 2、异常日志写入本地

---
+++ app/src/main/java/com/baorant/layoutdemo/activity/CrashHandlerActivity.java
@@ -0,0 +1,25 @@
+package com.baorant.layoutdemo.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baorant.layoutdemo.R;
+
+public class CrashHandlerActivity extends AppCompatActivity {
+    private static final String TAG = "CrashHandlerActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crash_handler);
+
+        Button crashBtn = findViewById(R.id.crashButton);
+        crashBtn.setOnClickListener(v -> {
+            Log.d(TAG, "crashBtn onclick");
+            throw new RuntimeException("手动抛出运行时异常");
+        });
+    }
+}
