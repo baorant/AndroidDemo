bbd6559d70040e91d24547bfd9529ea24e9b3062
baorant <779074463@qq.com>
2022-07-16 14:49:09 +0800

    [修改说明]增加视频播放页面 1、videoview播放视频 2、EXOplayer播放视频

---
+++ app/src/main/java/com/baorant/layoutdemo/activity/ExoplayerActivity.java
@@ -0,0 +1,75 @@
+package com.baorant.layoutdemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.baorant.layoutdemo.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+public class ExoplayerActivity extends AppCompatActivity {
+    private static final String TAG = "ExoplayerActivity";
+    PlayerView playerView;
+    private Uri uri;
+    SimpleExoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exoplayer);
+
+        playerView = findViewById(R.id.exoplayerView);
+
+        // 创建带宽
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // 创建轨道选择工厂
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        // 创建轨道选择实例
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        // 创建播放器实例
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        playerView.setPlayer(player);
+
+        // 创建加载数据的工厂
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, "baorant");
+        uri = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+        // 创建资源
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        // 准备播放
+        player.prepare(mediaSource);
+        // 开始播放
+        player.setPlayWhenReady(true);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
+            player.release();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.setPlayWhenReady(false);
+        }
+    }
+}
