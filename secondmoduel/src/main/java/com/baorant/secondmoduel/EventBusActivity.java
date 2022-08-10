a77747318e06df1f3cd31378965a1fa67bdb2701
baorant <779074463@qq.com>
2022-08-10 22:54:23 +0800

    [修改说明]路由页面跳转到secondModule,增加eventBus使用页面 接下来secondModule需要完成 （1）ButterKnife使用 （2）navigation使用（jetpeck） （3）data binding使用

---
+++ secondmoduel/src/main/java/com/baorant/secondmoduel/EventBusActivity.java
@@ -0,0 +1,34 @@
+package com.baorant.secondmoduel;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.baorant.frameworkmodule.activity.AbstractSubActivity;
+import com.baorant.frameworkmodule.msg.EventMessage;
+
+import org.greenrobot.eventbus.EventBus;
+
+@Route(path = "/second/EventBusActivity")
+public class EventBusActivity extends AbstractSubActivity {
+    private static final String TAG = "EventBusActivity";
+    private Button sendEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_bus);
+
+        sendEvent = findViewById(R.id.sendEvent);
+        sendEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "sendEvent onClick" );
+                EventMessage msg = new EventMessage(1,"Hello SecondMainActivity");
+                EventBus.getDefault().post(msg);
+            }
+        });
+    }
+}
--- secondmoduel/src/main/java/com/baorant/secondmoduel/SecondMainActivity.java
+++ secondmoduel/src/main/java/com/baorant/secondmoduel/SecondMainActivity.java
@@ -1,21 +1,56 @@
 package com.baorant.secondmoduel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.baorant.frameworkmodule.Util.JumpUtil;
 import com.baorant.frameworkmodule.activity.AbstractSubActivity;
+import com.baorant.frameworkmodule.msg.EventMessage;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 @Route(path = "/second/SecondMainActivity")
 public class SecondMainActivity extends AbstractSubActivity {
     private static final String TAG = "secondMainActivity";
+    private Button btnToEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "secondMainActivity onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_main);
+
+        btnToEvent = findViewById(R.id.btnToEvent);
+        btnToEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JumpUtil.jumpNextActivity(SecondMainActivity.this, EventBusActivity.class, "EventBusActivity页面");
+            }
+        });
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onReceiveMsg(EventMessage message) {
+        Log.d(TAG, "onReceiveMsg: " + message.toString());
+        Toast.makeText(SecondMainActivity.this, "this is SecondMainActivity show", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
     }
 }
