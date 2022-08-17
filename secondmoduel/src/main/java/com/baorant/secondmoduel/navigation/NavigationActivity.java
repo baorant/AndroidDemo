7f1800a075f4f399ee34f43d93b5caba1e36f00f
baorant <779074463@qq.com>
2022-08-17 22:23:01 +0800

    [修改说明]jetpeck navigation使用，和底部tab结合进行切换可以方便实现fragment之间的切换 (1)使用绑定navBtn和navControl的方式，快速绑定bottomNavigation实现fragment的切换 接下来secondModule需要完成 （1）data binding使用 （2）view model使用

--- secondmoduel/src/main/java/com/baorant/secondmoduel/navigation/NavigationActivity.java
+++ secondmoduel/src/main/java/com/baorant/secondmoduel/navigation/NavigationActivity.java
@@ -11,10 +11,11 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.baorant.frameworkmodule.activity.AbstractSubActivity;
 import com.baorant.secondmoduel.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends AbstractSubActivity {
     private static final String TAG = "NavigationActivity";
 
     @Override
@@ -32,21 +33,20 @@ public class NavigationActivity extends AppCompatActivity {
 
         // 这里如果我们需要对导航栏跳转时做判断，比如判断用户是否登录，未登录，就不跳转或者处理其他事件；
         // 这里返回true，则表示拦截不跳转，false则正常跳转；
-        navBtn.setOnNavigationItemSelectedListener(item -> {
-            Log.d(TAG, "onNavigationItemSelected");
-            int itemId = item.getItemId();
-            if (itemId == R.id.homefragment) {
-                navController.navigate(R.id.action_thirdFragment_to_firstFragment);
-            } else if (itemId == R.id.discoverfragment) {
-                navController.navigate(R.id.action_firstFragment_to_secondFragment);
-            } else if (itemId == R.id.mefragment) {
-                navController.navigate(R.id.action_secondFragment_to_thirdFragment);
-            }
-            // return true表示拦截，不让跳转，
-            return false;
-        });
-        // todo 绑定navBtn和navControl，不注释会报错，需要晚点研究
-//        NavigationUI.setupWithNavController(navBtn, navController);
-
+//        navBtn.setOnNavigationItemSelectedListener(item -> {
+//            Log.d(TAG, "onNavigationItemSelected");
+//            int itemId = item.getItemId();
+//            if (itemId == R.id.homefragment) {
+//                navController.navigate(R.id.action_thirdFragment_to_firstFragment);
+//            } else if (itemId == R.id.discoverfragment) {
+//                navController.navigate(R.id.action_firstFragment_to_secondFragment);
+//            } else if (itemId == R.id.mefragment) {
+//                navController.navigate(R.id.action_secondFragment_to_thirdFragment);
+//            }
+//            // return true表示拦截，不让跳转，
+//            return false;
+//        });
+        // 绑定navBtn和navControl，避免上面的方式调用，不注释会报错，需要晚点研究
+        NavigationUI.setupWithNavController(navBtn, navController);
     }
 }
