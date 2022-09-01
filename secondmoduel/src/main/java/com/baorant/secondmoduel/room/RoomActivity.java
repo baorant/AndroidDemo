74732c5df393f88a5d1f3c69b2b53d7257495a39
baorant <779074463@qq.com>
2022-09-01 23:20:25 +0800

    [修改说明]集成数据库操作room demo，使用handlerthread开启子线程操作room 接下来secondModule需要完成 （1）viewModel进阶使用，和livedata结合使用原理分析，以及room的实现原理分析 （2）page分页集成 （3）workmanager集成使用

--- secondmoduel/src/main/java/com/baorant/secondmoduel/room/RoomActivity.java
+++ secondmoduel/src/main/java/com/baorant/secondmoduel/room/RoomActivity.java
@@ -3,10 +3,14 @@ package com.baorant.secondmoduel.room;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,25 +37,38 @@ public class RoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
 
+        initView();
+
         init();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void init() {
+    private void initView() {
         addAge = findViewById(R.id.addAge);
         addName = findViewById(R.id.addName);
         addStudent = findViewById(R.id.addBtn);
         queryStudents = findViewById(R.id.queryBtn);
         showStudents = findViewById(R.id.studentInfo);
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void init() {
         studentDao = MyDatabase.getInstance(RoomActivity.this).studentDao();
 
         studentInfoHandler = new Handler();
 
-        addStudent.setOnClickListener(view -> {
+        // 使用handler启动子线程
+        HandlerThread handlerThread = new HandlerThread("my thread");
+        handlerThread.start();
+        Handler myHandler = new Handler(handlerThread.getLooper(), msg -> {
             String age = addAge.getText().toString();
             String name = addName.getText().toString();
-            new Thread(() -> studentDao.insertStudent(new Student(name, age))).start();
+            studentDao.insertStudent(new Student(name, age));
+            return true;
+        });
+
+        addStudent.setOnClickListener(view -> {
+//            new Thread(() -> studentDao.insertStudent(new Student(name, age))).start();
+            myHandler.sendEmptyMessage(0);
         });
 
         final String[] info = {""};
